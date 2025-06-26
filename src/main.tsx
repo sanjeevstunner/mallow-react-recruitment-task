28f9e12dd2e2a6321306f9440d559fa04b3116ce
Sanjeev <sanjeevstunner1@gmail.com>
2025-06-26 22:45:26 +0530

    setup redux store
    Signed-off-by: Sanjeev <sanjeevstunner1@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './features/store'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
