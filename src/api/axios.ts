54e86b39b6df5a3f5d9303a812d09c589085f0b6
Sanjeev <sanjeevstunner1@gmail.com>
2025-06-26 22:44:43 +0530

    setup centralized axios service for resuable API fetching
    Signed-off-by: Sanjeev <sanjeevstunner1@gmail.com>

---
+++ src/api/axios.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+class AxiosService {
+  private static instance: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    timeout: 1000 * 30,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  static get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.instance.get<T>(url, config);
+  }
+
+  static post<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.instance.post<T>(url, data, config);
+  }
+
+  static put<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.instance.put<T>(url, data, config);
+  }
+
+  static patch<T = unknown>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.instance.patch<T>(url, data, config);
+  }
+
+  static delete<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.instance.delete<T>(url, config);
+  }
+}
+
+export default AxiosService; 
